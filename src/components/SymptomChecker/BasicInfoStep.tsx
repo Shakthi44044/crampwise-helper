7250541722b85432bf28b1e3bcca66f6bce7d436
shakthic <shakthic44044@gmail.com>
2025-03-04 21:21:32 +0530

    Update BasicInfoStep.tsx

--- src/components/SymptomChecker/BasicInfoStep.tsx
+++ src/components/SymptomChecker/BasicInfoStep.tsx
@@ -104,12 +104,12 @@ const BasicInfoStep = ({
       </div>
 
       <div className="text-center mt-6">
-        <Button
+        {/* <Button
           onClick={onShowResults}
           className="px-4 py-2 bg-blue-600 text-white rounded-lg shadow hover:bg-blue-700"
         >
           Show Results
-        </Button>
+        </Button> */}
       </div>
     </div>
   );
